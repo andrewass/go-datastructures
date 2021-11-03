71f8647d8d400d7f25d99887f931d6f064dc2b8c
Andreas Wassum <a.wassum@gmail.com>
2021-11-03 21:36:11 +0100

    Added BitSet

---
+++ misc/bitset/bitset.go
@@ -0,0 +1,56 @@
+package bitset
+
+type BitSet struct {
+	list []bool
+	size int
+}
+
+func New(size int) *BitSet{
+	return &BitSet{size: size, list: make([]bool, size)}
+}
+
+func (bs *BitSet) Get(index int) bool {
+	return bs.list[index]
+}
+
+func (bs *BitSet) Set(index int) {
+	bs.list[index] = true
+}
+
+func (bs *BitSet) SetRange(from int, to int) {
+	for i := from; i < to; i++ {
+		bs.list[i] = true
+	}
+}
+
+func (bs *BitSet) Clear(index int) {
+	bs.list[index] = false
+}
+
+func (bs *BitSet) ClearRange(from int, to int) {
+	for i := from; i < to; i++ {
+		bs.list[i] = false
+	}
+}
+
+func (bs *BitSet) Flip(index int) {
+	bs.list[index] = !bs.list[index]
+}
+
+func (bs *BitSet) FlipRange(from int, to int) {
+	for i := from; i < to; i++ {
+		bs.list[i] = !bs.list[i]
+	}
+}
+
+func (bs *BitSet) Cardinality() int {
+	setBits := 0
+	for i := 0; i < bs.size; i++ {
+		if bs.list[i] {
+			setBits++
+		}
+	}
+	return setBits
+}
+
+
---
+++ misc/bitset/bitset_test.go
@@ -0,0 +1,73 @@
+package bitset
+
+import "testing"
+
+func TestShouldSetExpectedBit(t *testing.T)  {
+	bitSet := New(20)
+	bitSet.Set(4)
+
+	if !bitSet.Get(4) {
+		t.Error("Bit at index 4 should be set")
+	}
+}
+
+func TestShouldClearExpectedBit(t *testing.T) {
+	bitSet := New(20)
+	bitSet.Set(4)
+	bitSet.Clear(4)
+
+	if bitSet.Get(4) {
+		t.Error("Bit at index 4 should be cleared")
+	}
+}
+
+func TestShouldFlipExpectedBit(t *testing.T)  {
+	bitSet := New(20)
+
+	bitSet.Flip(4)
+	if !bitSet.Get(4) {
+		t.Error("Bit at index 4 should be set")
+	}
+
+	bitSet.Flip(4)
+	if bitSet.Get(4) {
+		t.Error("Bit at index 4 should be cleared")
+	}
+}
+
+func TestShouldSetRangeOfBits(t *testing.T)  {
+	bitSet := New(20)
+	bitSet.SetRange(3, 10)
+
+	setCount := bitSet.Cardinality()
+	if setCount != 7 {
+		t.Error("Number of set bits should be 7, got ",setCount)
+	}
+}
+
+func TestShouldClearRangeOfBits(t *testing.T)  {
+	bitSet := New(20)
+	bitSet.SetRange(0, 20)
+	bitSet.ClearRange(5, 10)
+
+	setCount := bitSet.Cardinality()
+	if setCount != 15 {
+		t.Error("Number of set bits should be 15, got ",setCount)
+	}
+}
+
+func TestShouldFlipRangeOfBits(t *testing.T)  {
+	bitSet := New(20)
+
+	bitSet.FlipRange(5, 10)
+	setCount := bitSet.Cardinality()
+	if setCount != 5 {
+		t.Error("Number of set bits should be 5, got ",setCount)
+	}
+
+	bitSet.FlipRange(5, 10)
+	setCount = bitSet.Cardinality()
+	if setCount != 0 {
+		t.Error("Number of set bits should be 0, got ",setCount)
+	}
+}
