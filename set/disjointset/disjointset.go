ad64c3c0c0d4852aa97cd4757fa45270fadbbe83
Andreas Wassum <a.wassum@gmail.com>
2021-11-10 21:14:12 +0100

    Added more functionality to disjoint set

--- set/disjointset/disjointset.go
+++ set/disjointset/disjointset.go
@@ -1,25 +1,68 @@
 package disjointset
 
 type setNode struct {
-	item interface{}
+	item   interface{}
+	rank   uint
+	size   uint
+	parent *setNode
 }
 
 type DisjointSet struct {
-	setNodes []*setNode
+	nodeSet  []*setNode
+	mapNodes map[interface{}]*setNode
 }
 
 func New() *DisjointSet {
-	return &DisjointSet{}
+	return &DisjointSet{mapNodes: make(map[interface{}]*setNode)}
 }
 
-func (djs *DisjointSet) AddItem(item interface{}) {
-	djs.setNodes = append(djs.setNodes, &setNode{item: item})
+func (djs *DisjointSet) AddItem(key interface{}, item interface{}) {
+	newNode := &setNode{item: item, size: 1}
+	djs.nodeSet = append(djs.nodeSet, newNode)
+	djs.mapNodes[key] = newNode
 }
 
-func (djs *DisjointSet) Union(x *setNode, y *setNode) {
-
+func (djs *DisjointSet) GetItem(key interface{}) interface{} {
+	node := djs.mapNodes[key]
+	if node == nil {
+		return node
+	} else {
+		return node.item
+	}
 }
 
-func (djs *DisjointSet) GetItem(index int) *setNode {
-	return djs.setNodes[index]
+func (djs *DisjointSet) Union(keyA interface{}, keyB interface{}) {
+	rootA := djs.findRootNode(djs.mapNodes[keyA])
+	rootB := djs.findRootNode(djs.mapNodes[keyB])
+
+	if rootA == rootB {
+		return
+	}
+	if rootA.rank > rootB.rank {
+		rootB.parent = rootA
+		rootA.size += rootB.size
+	} else {
+		rootA.parent = rootB
+		rootB.size += rootA.size
+		if rootB.rank == rootA.rank {
+			rootB.rank++
+		}
+	}
+}
+
+func (djs *DisjointSet) IsSameSet(keyA interface{}, keyB interface{}) bool {
+	return djs.findRootNode(djs.mapNodes[keyA]) == djs.findRootNode(djs.mapNodes[keyB])
+}
+
+func (djs *DisjointSet) GetSetSize(key interface{}) uint {
+	rootNode := djs.findRootNode(djs.mapNodes[key])
+
+	return rootNode.size
+}
+
+func (djs *DisjointSet) findRootNode(node *setNode) *setNode {
+	if node.parent != nil{
+		node = djs.findRootNode(node.parent)
+	}
+	return node
 }
--- set/disjointset/disjointset_test.go
+++ set/disjointset/disjointset_test.go
@@ -2,11 +2,45 @@ package disjointset
 
 import "testing"
 
-func TestShouldCreateDisjointSet(t *testing.T)  {
-	disjointSet := New()
-
-	disjointSet.AddItem(3)
-	disjointSet.AddItem(5)
-	disjointSet.AddItem(7)
+type testStruct struct {
+	id   int
+	name string
 }
 
+func TestShouldCreateDisjointSet(t *testing.T) {
+	disjointSet := New()
+	items := []*testStruct{{id: 1, name: "firstItem"}, {id: 2, name: "secondItem"}, {id: 3, name: "thirdItem"},
+		{id: 4, name: "fourthItem"}, {id: 5, name: "fifthItem"}, {id: 6, name: "sixthItem"}}
+
+	for _, item := range items {
+		disjointSet.AddItem(item.id, item)
+	}
+
+	if fetchedItem := disjointSet.GetItem(items[2].id); fetchedItem != items[2] {
+		t.Error("Should get item 3, got", fetchedItem.(testStruct))
+	}
+
+	if !disjointSet.IsSameSet(items[0].id, items[0].id) {
+		t.Error("Item should be in same set as itself")
+	}
+
+	disjointSet.Union(items[1].id, items[3].id)
+	disjointSet.Union(items[3].id, items[4].id)
+	disjointSet.Union(items[1].id, items[5].id)
+
+	if !disjointSet.IsSameSet(items[3].id, items[5].id) {
+		t.Error("Item 4 and 6 should be in same set")
+	}
+
+	if disjointSet.IsSameSet(items[2].id, items[4].id) {
+		t.Error("Item 3 and 5 should not be in same set")
+	}
+
+	if setSize := disjointSet.GetSetSize(items[3].id); setSize != 4 {
+		t.Error("Set size should be 4, got", setSize)
+	}
+
+	if setSize := disjointSet.GetSetSize(items[2].id); setSize != 1 {
+		t.Error("Set size should be 1, got", setSize)
+	}
+}
